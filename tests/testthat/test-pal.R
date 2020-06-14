71d3b2eec493e875ec776becee87ac64d6601e0a
Salim B <salim@posteo.de>
2020-06-14 20:10:46 +0200

    delete file left-overs from test

--- tests/testthat/test-pal.R
+++ tests/testthat/test-pal.R
@@ -2,7 +2,24 @@
 
 
 # R Packages ----
+test_that("`README.Rmd` can be built successfully.", {
 
+  readr::write_file(x = "---
+output: github_document
+---
+
+# hi there
+
+nothing `r 'here.'`",
+                    path = "README.Rmd")
+
+  pal::build_readme()
+
+  expect_match(object = readr::read_file("README.md"),
+               regexp = "nothing here\\.")
+
+  fs::file_delete(path = c("README.md",
+                           "README.Rmd"))
+})
 
 # Miscellaneous ----
-
