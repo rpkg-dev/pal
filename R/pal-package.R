b972c07857a09ed26e5b5efac27b4339f64610a0
Salim B <git@salim.space>
2024-05-27 23:30:47 +0200

    chore: update lifecycle integration

--- R/pal-package.R
+++ R/pal-package.R
@@ -10,6 +10,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @importFrom lifecycle deprecate_soft
+#' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
